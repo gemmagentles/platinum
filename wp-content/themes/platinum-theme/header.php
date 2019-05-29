32ad8e9888e12d38b8f743d4df025fde0c9f8c17
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-05-28 18:42:29 -0700

    build a new acf to work as a custom menu. This will allow to add images as backgrounds to the drop down menu. Make it a global site option so it is seperate from everything else and easy to access in the dashboard. Include footer items in global options.

--- wp-content/themes/platinum-theme/header.php
+++ wp-content/themes/platinum-theme/header.php
@@ -15,12 +15,59 @@
 <body <?php body_class(); ?>>
 <div class="wrapper">
     <header class="header clear" role="banner">
-        <div class="logo">
+        <div class="header__logo">
             <a href="<?php echo home_url(); ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Platinum Companies" class="logo-img">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Platinum Companies" class="header__logo-img">
             </a>
         </div>
-        <nav class="nav" role="navigation">
-            <?php html5blank_nav(); ?>
+        <nav class="header__nav" role="navigation">
+            <?php //html5blank_nav(); ?>
+
+            <!-- Menu -->
+            <?php if ( have_rows( 'menu', 'option' ) ) : ?>
+                <?php while ( have_rows( 'menu', 'option' ) ) : the_row(); ?>
+                    <a class="header__nav--link" href="<?php the_sub_field( 'menu_item_link' ); ?>"><?php the_sub_field( 'menu_item_name' ); ?></a>
+                    
+                    <?php if ( get_sub_field( 'drop_down_option' ) == 1 ) { 
+                    // echo 'true'; 
+                    } else { 
+                    // echo 'false'; 
+                    } ?>
+                    <?php if ( have_rows( 'drop_down_menu' ) ) : ?>
+                        <div class="header__dropdown--wrapper">
+                            <?php while ( have_rows( 'drop_down_menu' ) ) : the_row(); ?>
+                                <div class="header__dropdown--container">
+                                    <a class="header__dropdown--link" title="<?php the_sub_field( 'sub_menu_item_name' ); ?>" href="<?php the_sub_field( 'sub_menu_item_link' ); ?>"><div class="header__dropdown--text"><?php the_sub_field( 'sub_menu_item_name' ); ?></div>
+                                    <?php if ( get_sub_field( 'button_image' ) ) { ?>
+                                        <img class="header__dropdown--image" src="<?php the_sub_field( 'button_image' ); ?>" />
+                                    <?php } ?>
+                                    </a>
+                                </div>
+                            <?php endwhile; ?>
+                        </div>
+                    <?php else : ?>
+                        <?php // no rows found ?>
+                    <?php endif; ?>
+                <?php endwhile; ?>
+            <?php else : ?>
+                <?php // no rows found ?>
+            <?php endif; ?>
+
         </nav>
+
+        <div class="header__social">
+
+
+                <a class="header__social--link" title="Facebook" href="<?php the_field('facebook_url', 'option'); ?>" target="_blank">
+                    <svg class="header__social--icon"><use href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-facebook-icon" xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-facebook-icon"/></svg>
+                </a>
+                <a class="header__social--link" title="Twitter" href="<?php the_field('twitter_url', 'option'); ?>" target="_blank">
+                    <svg class="header__social--icon"><use href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-twitter-icon" xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-twitter-icon"/></svg>
+                </a>
+                <a class="header__social--link" title="Instagram" href="<?php the_field('instagram_url', 'option'); ?>" target="_blank">
+                    <svg class="header__social--icon"><use href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-instagram-icon" xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-instagram-icon"/></svg>
+                </a>
+
+
+        </div>
     </header>
