408377c7b55db9ca87e63bc6804ad1c9ff7798b9
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-05-31 14:26:50 -0700

    Add a container div around the header so the header background can go 100% width but the inner part stays centered with a max width. Add styles to target all other pages not home page, and style the nav bar with block color. Replace the social svg icons with cleaner coded svgs so they now appear.

--- wp-content/themes/platinum-theme/header.php
+++ wp-content/themes/platinum-theme/header.php
@@ -15,49 +15,47 @@
 <body <?php body_class(); ?>>
 <div class="wrapper">
     <header class="header clear" role="banner">
-        <div class="header__logo">
-            <a href="<?php echo home_url(); ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Platinum Companies" class="header__logo-img">
-            </a>
-        </div>
-        <nav class="header__nav" role="navigation">
-            <?php //html5blank_nav(); ?>
+        <div class="header__container">
+            <div class="header__logo">
+                <a href="<?php echo home_url(); ?>">
+                    <svg class="header__logo-img"><use href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-logo" xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-logo"/></svg>
+                </a>
+            </div>
+            <nav class="header__nav" role="navigation">
+                <?php //html5blank_nav(); ?>
 
-            <!-- Menu -->
-            <?php if ( have_rows( 'menu', 'option' ) ) : ?>
-                <?php while ( have_rows( 'menu', 'option' ) ) : the_row(); ?>
-                    <a class="header__nav--link" href="<?php the_sub_field( 'menu_item_link' ); ?>"><?php the_sub_field( 'menu_item_name' ); ?></a>
-                    
-                    <?php if ( get_sub_field( 'drop_down_option' ) == 1 ) { 
-                    // echo 'true'; 
-                    } else { 
-                    // echo 'false'; 
-                    } ?>
-                    <?php if ( have_rows( 'drop_down_menu' ) ) : ?>
-                        <div class="header__dropdown--wrapper">
-                            <?php while ( have_rows( 'drop_down_menu' ) ) : the_row(); ?>
-                                <div class="header__dropdown--container">
-                                    <a class="header__dropdown--link" title="<?php the_sub_field( 'sub_menu_item_name' ); ?>" href="<?php the_sub_field( 'sub_menu_item_link' ); ?>"><div class="header__dropdown--text"><?php the_sub_field( 'sub_menu_item_name' ); ?></div>
-                                    <?php if ( get_sub_field( 'button_image' ) ) { ?>
-                                        <img class="header__dropdown--image" src="<?php the_sub_field( 'button_image' ); ?>" />
-                                    <?php } ?>
-                                    </a>
-                                </div>
-                            <?php endwhile; ?>
-                        </div>
-                    <?php else : ?>
-                        <?php // no rows found ?>
-                    <?php endif; ?>
-                <?php endwhile; ?>
-            <?php else : ?>
-                <?php // no rows found ?>
-            <?php endif; ?>
+                <!-- Menu -->
+                <?php if ( have_rows( 'menu', 'option' ) ) : ?>
+                    <?php while ( have_rows( 'menu', 'option' ) ) : the_row(); ?>
+                        <a class="header__nav--link" href="<?php the_sub_field( 'menu_item_link' ); ?>"><?php the_sub_field( 'menu_item_name' ); ?></a>
+                        <?php if ( get_sub_field( 'drop_down_option' ) == 1 ) { 
+                        // echo 'true'; 
+                        } else { 
+                        // echo 'false'; 
+                        } ?>
+                        <?php if ( have_rows( 'drop_down_menu' ) ) : ?>
+                            <div class="header__dropdown--wrapper">
+                                <?php while ( have_rows( 'drop_down_menu' ) ) : the_row(); ?>
+                                    <div class="header__dropdown--container">
+                                        <a class="header__dropdown--link" title="<?php the_sub_field( 'sub_menu_item_name' ); ?>" href="<?php the_sub_field( 'sub_menu_item_link' ); ?>">
+                                            <?php if ( get_sub_field( 'button_image' ) ) { ?>
+                                                <img class="header__dropdown--image" src="<?php the_sub_field( 'button_image' ); ?>" />
+                                            <?php } ?>
+                                            <div class="header__dropdown--text"><?php the_sub_field( 'sub_menu_item_name' ); ?></div>
+                                        </a>
+                                    </div>
+                                <?php endwhile; ?>
+                            </div>
+                        <?php else : ?>
+                            <?php // no rows found ?>
+                        <?php endif; ?>
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <?php // no rows found ?>
+                <?php endif; ?>                         
+            </nav>
 
-        </nav>
-
-        <div class="header__social">
-
-
+            <div class="header__social">
                 <a class="header__social--link" title="Facebook" href="<?php the_field('facebook_url', 'option'); ?>" target="_blank">
                     <svg class="header__social--icon"><use href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-facebook-icon" xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-facebook-icon"/></svg>
                 </a>
@@ -67,7 +65,6 @@
                 <a class="header__social--link" title="Instagram" href="<?php the_field('instagram_url', 'option'); ?>" target="_blank">
                     <svg class="header__social--icon"><use href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-instagram-icon" xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/icons.svg#icon-instagram-icon"/></svg>
                 </a>
-
-
+            </div>
         </div>
     </header>
