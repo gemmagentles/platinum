0197a71865c58eba3cc1177b984878a8957d8b62
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-06-05 18:33:52 -0700

    style footer component. Pull in global options variables phone, email and social.

--- wp-content/themes/platinum-theme/functions.php
+++ wp-content/themes/platinum-theme/functions.php
@@ -155,6 +155,31 @@ function html5blank_nav()
     );
 }
 
+// footer navigation
+function footer_nav()
+{
+    wp_nav_menu(
+        array(
+            'theme_location' => 'extra-menu',
+            'menu' => '',
+            'container' => 'div',
+            'container_class' => 'menu-{menu slug}-container',
+            'container_id' => '',
+            'menu_class' => 'menu',
+            'menu_id' => '',
+            'echo' => true,
+            'fallback_cb' => 'wp_page_menu',
+            'before' => '',
+            'after' => '',
+            'link_before' => '',
+            'link_after' => '',
+            'items_wrap' => '<ul>%3$s</ul>',
+            'depth' => 0,
+            'walker' => ''
+        )
+    );
+}
+
 // Load HTML5 Blank scripts (header.php)
 function html5blank_header_scripts()
 {
