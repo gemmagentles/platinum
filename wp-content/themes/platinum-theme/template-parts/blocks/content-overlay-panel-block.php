0635b4b5b1809f54a1a98112a84ce0d09a881e66
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-06-20 16:47:51 -0700

    make animations faster.

--- wp-content/themes/platinum-theme/template-parts/blocks/content-overlay-panel-block.php
+++ wp-content/themes/platinum-theme/template-parts/blocks/content-overlay-panel-block.php
@@ -9,9 +9,9 @@
                     <?php } ?>
                 </div>
                 <div class="content-overlay-panel__text-wrapper">
-                    <span data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="600" data-aos-duration="550" class="subtitle"><?php the_sub_field( 'subtitle' ); ?></span>
-                    <h2 data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="800" data-aos-duration="550" class="content-overlay-panel__heading"><?php the_sub_field( 'heading' ); ?></h2>
-                    <div data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="1000" data-aos-duration="550" class="content-overlay-panel__paragraph"><?php the_sub_field( 'paragraph' ); ?></div>
+                    <span data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="400" data-aos-duration="550" class="subtitle"><?php the_sub_field( 'subtitle' ); ?></span>
+                    <h2 data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="600" data-aos-duration="550" class="content-overlay-panel__heading"><?php the_sub_field( 'heading' ); ?></h2>
+                    <div data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="800" data-aos-duration="550" class="content-overlay-panel__paragraph"><?php the_sub_field( 'paragraph' ); ?></div>
                     <?php $button_link = get_sub_field( 'button_link' ); ?>
                     <?php if ( $button_link ) { ?>
                         <button data-aos="fade" data-aos-easing="ease-in" data-aos-delay="500" data-aos-duration="550" class="platinum-button" onclick="location.href='<?php echo $button_link['url']; ?>'" type="button"><?php the_sub_field( 'button_text' ); ?></button>
--- wp-content/themes/platinum-theme/template-parts/blocks/map-block.php
+++ wp-content/themes/platinum-theme/template-parts/blocks/map-block.php
@@ -6,9 +6,9 @@
                     <?php the_sub_field( 'map_shortcode' ); ?>
                 </div>
                 <div class="map__content-box">
-                    <span data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="600" data-aos-duration="550" class="subtitle"><?php the_sub_field( 'subtitle' ); ?></span>
-                    <h2 data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="800" data-aos-duration="550" class="map__heading"><?php the_sub_field( 'heading' ); ?></h2>
-                    <div data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="1000" data-aos-duration="550" class="map__paragraph"><?php the_sub_field( 'paragraph' ); ?></div>
+                    <span data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="400" data-aos-duration="550" class="subtitle"><?php the_sub_field( 'subtitle' ); ?></span>
+                    <h2 data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="600" data-aos-duration="550" class="map__heading"><?php the_sub_field( 'heading' ); ?></h2>
+                    <div data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="800" data-aos-duration="550" class="map__paragraph"><?php the_sub_field( 'paragraph' ); ?></div>
                 </div>
             <?php endwhile; ?>
         </div>
