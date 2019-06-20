2d3ab5e57ae33eeb5c0ce1469743214b85df51cc
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-06-19 21:09:13 -0700

    installed aos plugin for scroll animation. Add classes into each component to detect what items should animate.

--- wp-content/themes/platinum-theme/header.php
+++ wp-content/themes/platinum-theme/header.php
@@ -8,6 +8,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.1/min/tiny-slider.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.1/tiny-slider.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.1/tiny-slider.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.1/min/tiny-slider.js"></script>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
--- wp-content/themes/platinum-theme/template-parts/blocks/half-and-half-panel-block.php
+++ wp-content/themes/platinum-theme/template-parts/blocks/half-and-half-panel-block.php
@@ -3,18 +3,18 @@
 	<?php while ( have_rows( 'half_and_half_panel_repeater' ) ) : the_row(); ?>
     <div class="half-and-half-panel__wrapper<?php if ( get_sub_field( 're-order' ) == 1 ) { ?> half-and-half-panel__order-left<?php } ?>">
             <div class="half-and-half-panel__text-wrapper">
-                <span class="subtitle"><?php the_sub_field( 'sub_title' ); ?></span>
-                <h2 class="half-and-half-panel__heading"><?php the_sub_field( 'heading' ); ?></h2>
-                <div class="half-and-half-panel__paragraph"><?php the_sub_field( 'paragraph' ); ?></div>
+                <span data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="50" data-aos-duration="550" class="subtitle"><?php the_sub_field( 'sub_title' ); ?></span>
+                <h2 data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="250" data-aos-duration="550" class="half-and-half-panel__heading"><?php the_sub_field( 'heading' ); ?></h2>
+                <div data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="450" data-aos-duration="550" class="half-and-half-panel__paragraph"><?php the_sub_field( 'paragraph' ); ?></div>
                 <?php $button_link = get_sub_field( 'button_link' ); ?>
                 <?php if ( $button_link ) { ?>
-                    <button class="platinum-button" onclick="location.href='<?php echo $button_link['url']; ?>'" type="button"><?php the_sub_field( 'button_text' ); ?></button>
+                    <button data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="500" data-aos-duration="550" class="platinum-button" onclick="location.href='<?php echo $button_link['url']; ?>'" type="button"><?php the_sub_field( 'button_text' ); ?></button>
                 <?php } ?>
             </div>
             <div class="half-and-half-panel__image-wrapper">
                 <?php $image = get_sub_field( 'image' ); ?>
                 <?php if ( $image ) { ?>
-                    <img class="half-and-half-panel__image" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                    <img data-aos="fade-right" data-aos-easing="ease-in" data-aos-delay="650" data-aos-duration="550" class="half-and-half-panel__image" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
                 <?php } ?>
             </div>
         </div>
