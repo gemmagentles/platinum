6f112338e02048acf84fb0c8838403e34a90a36e
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-09-05 16:56:51 -0700

    Comment out the hover effect while we are waiting for content to be added to this. Add an optional if statment around the paragraph that is in the hover so it doesnt get shown if there is no paragraph text.

--- wp-content/themes/platinum-theme/template-parts/blocks/cards-with-captions-block.php
+++ wp-content/themes/platinum-theme/template-parts/blocks/cards-with-captions-block.php
@@ -6,9 +6,14 @@
             <?php if ( $image ) { ?>
                 <div class="cards-with-captions__image-wrapper">
                     <img data-aos="fade-up" data-aos-easing="ease-in" data-aos-duration="550" class="cards-with-captions__image" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                    <div class="cards-with-captions__hover-paragraph">
-                        <?php the_sub_field( 'hover_paragraph' ); ?>
-                    </div>
+                    
+                    <?php $hover_paragraph = get_sub_field( 'hover_paragraph' ); ?>
+
+                    <?php if ( $hover_paragraph ) { ?>
+                        <div class="cards-with-captions__hover-paragraph">
+                            <?php the_sub_field( 'hover_paragraph' ); ?>
+                        </div>
+                    <?php } ?>
                 </div>
             <?php } ?>
             <h3 class="cards-with-captions__title"><?php the_sub_field( 'title' ); ?></h3>
